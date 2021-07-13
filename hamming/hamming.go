a1fbb3ae69a4f8456d3b113a397f1b7f3f452f84
Aabishkar Aryal <45176384+aabishkaryal@users.noreply.github.com>
2021-07-13 15:11:13 +0000

    Refactor: hamming

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -9,7 +9,7 @@ func Distance(a, b string) (int, error) {
 	}
 	d := 0
 	s1, s2 := []rune(a), []rune(b)
-	for i, _ := range s1 {
+	for i := range s1 {
 		c1, c2 := s1[i], s2[i]
 		if c1 != c2 {
 			d++
