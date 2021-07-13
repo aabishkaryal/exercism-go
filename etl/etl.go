2dcbe5816f9d6d8c62d36ff4e4509466a9cac169
Aabishkar Aryal <45176384+aabishkaryal@users.noreply.github.com>
2021-07-13 15:09:47 +0000

    Documentation: etl

--- etl/etl.go
+++ etl/etl.go
@@ -2,6 +2,7 @@ package etl
 
 import "strings"
 
+// Function Transform transforms the given map from key, [values] to value, key
 func Transform(old_map map[int][]string) map[string]int {
 	result := make(map[string]int)
 	for k, v := range old_map {
