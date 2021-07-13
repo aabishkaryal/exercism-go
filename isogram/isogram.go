5c6664659a108dfac5123a227916d62b5d3f02c3
Aabishkar Aryal <45176384+aabishkaryal@users.noreply.github.com>
2021-07-13 15:13:29 +0000

    Documentation: isogram

--- isogram/isogram.go
+++ isogram/isogram.go
@@ -4,6 +4,7 @@ import "strings"
 
 const letters string = "abcdefghijklmnopqrstuvwxyz"
 
+// Function IsIsogram returns boolean indicating if the given string is isogram.
 func IsIsogram(word string) bool {
 	word = strings.ToLower(word)
 	for _, letter := range letters {
