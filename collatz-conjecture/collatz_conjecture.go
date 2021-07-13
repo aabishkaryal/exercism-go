54e1847d8ced1f22537fdf2c79ca1e802aef83ac
Aabishkar Aryal <45176384+aabishkaryal@users.noreply.github.com>
2021-07-13 15:02:56 +0000

    Complete: collatz_conjecture

---
+++ collatz-conjecture/collatz_conjecture.go
@@ -0,0 +1,21 @@
+package collatzconjecture
+
+import "errors"
+
+// Function CollatzConjecture returns the number of steps
+// needed for n to reach 1 in a colltaz series.
+func CollatzConjecture(n int) (int, error) {
+	if n <= 0 {
+		return 0, errors.New("provided number should be non-zero positive number")
+	}
+	step := 0
+	for n > 1 {
+		if n%2 == 0 {
+			n = n / 2
+		} else {
+			n = 3*n + 1
+		}
+		step++
+	}
+	return step, nil
+}
